ee2e5de67c9d753c8f65ae2fb2bf739639a4c201
Pedro Henrique <pedrosilva101001@gmail.com>
2026-08-06 20:44:31 -0300

    feat: cria infra de persistencia de User

--- src/main/java/com/pedrohenrique/library_management_api/core/application/contracts/repositories/UserRepository.java
+++ src/main/java/com/pedrohenrique/library_management_api/core/application/contracts/repositories/UserRepository.java
@@ -1,19 +1,19 @@
 package com.pedrohenrique.library_management_api.core.application.contracts.repositories;
 
 import com.pedrohenrique.library_management_api.core.domain.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-public interface UserRepository extends JpaRepository <User, Long> {
+
+public interface UserRepository  {
     User save(User user);
     Optional<User> findById(Long id);
-    Optional<User> findByEmail(String Email);
+    Optional<User> findByEmail(String email);
     Boolean existsByCpf(String cpf);
     Boolean existsByEmail(String email);
     List<User>findAll();
-    void delete(User user);
+    void delete(Long id);
+
 }
---
+++ src/main/java/com/pedrohenrique/library_management_api/infra/persistence/UserMapper.java
@@ -0,0 +1,22 @@
+package com.pedrohenrique.library_management_api.infra.persistence;
+
+import com.pedrohenrique.library_management_api.core.domain.entities.User;
+
+public class UserMapper {
+
+    public static User toDomain(UserJpaEntity jpa){
+        return new User(
+                jpa.getId(), jpa.getName(), jpa.getCpf(), jpa.getPhone(), jpa.getPassword(), jpa.getEmail(),
+                jpa.getBirthDate(), jpa.getRegistrationDate(), jpa.getActive(),
+                jpa.getAddressId(), jpa.getRole(), jpa.getProfileId(), jpa.getCustomLoanLimit()
+        );
+    }
+
+    public static UserJpaEntity toJpaEntity(User domain){
+        return new UserJpaEntity(
+                domain.getId(), domain.getName(), domain.getCpf(), domain.getPhone(), domain.getPassword(), domain.getEmail(),
+                domain.getBirthDate(), domain.getRegistrationDate(), domain.isActive(),
+                domain.getAddressId(), domain.getRole(), domain.getProfileId(), domain.getCustomLoanLimit()
+        );
+    }
+}
