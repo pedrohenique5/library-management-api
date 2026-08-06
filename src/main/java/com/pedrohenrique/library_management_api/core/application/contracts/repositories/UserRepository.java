dafe83fd54b5b2d9414a253bc93972947bc4a559
Pedro Henrique <pedrosilva101001@gmail.com>
2026-08-05 21:02:17 -0300

    feat: cria dtos de autenticacao

--- src/main/java/com/pedrohenrique/library_management_api/core/application/contracts/repositories/UserRepository.java
+++ src/main/java/com/pedrohenrique/library_management_api/core/application/contracts/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package com.pedrohenrique.library_management_api.core.application.contracts.repo
 
 import com.pedrohenrique.library_management_api.core.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository <User, Long> {
     User save(User user);
     Optional<User> findById(Long id);
